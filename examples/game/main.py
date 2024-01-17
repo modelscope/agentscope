60c89ccd8fa5999c329ae6e669607a53019e77d3
weirui.kwr@alibaba-inc.com <weirui.kwr@alibaba-inc.com>
2024-01-17 11:53:50 +0800

    update

--- examples/game/main.py
+++ examples/game/main.py
@@ -54,8 +54,9 @@ def invited_group_chat(invited_customer, player, cur_plots_indices):
     invited_names.sort()
     print(cur_plots_indices)
     for idx in cur_plots_indices:
-        correct_names = [GAME_CONFIG["plots"][idx]["main_role"]] + \
-                        GAME_CONFIG["plots"][idx]["supporting_roles"]
+        correct_names = [GAME_CONFIG["plots"][idx]["main_role"]] + GAME_CONFIG[
+            "plots"
+        ][idx]["supporting_roles"]
         correct_names.sort()
         print("current names", correct_names)
 
@@ -83,7 +84,13 @@ def one_on_one_loop(customers, player):
     visit_customers = [c for c in customers if c.visit()]
     random.shuffle(visit_customers)
 
-    ingredients = yaml.safe_load(open("config/ingredients.yaml"))
+    with open(
+        "config/ingredients.yaml",
+        "r",
+        encoding="utf-8",
+    ) as ingredients_file:
+        ingredients = yaml.safe_load(ingredients_file)
+
     ingredient_today = {}
     for category, items in ingredients.items():
         ingredient_today[category] = (
@@ -112,7 +119,7 @@ def one_on_one_loop(customers, player):
             msg = customer(msg)
             if "score" in msg:
                 print(
-                    f"【系统】{customer.name}（顾客）接受了你的推荐。\n"
+                    f"【系统】{customer.name}（顾客）接受了你的菜。\n"
                     f"【系统】顾客对菜本身的评价：{msg['content']}\n"
                     f"【系统】{customer.name}（顾客）享用完之后，"
                     f"综合满意度为{msg['score']}\n",
@@ -120,8 +127,7 @@ def one_on_one_loop(customers, player):
                 break
             speak_print(msg)
             print(
-                "【系统】如果想要根据顾客口味做菜，请说“做菜” 否则请不要包含“做菜”关键词。"
-                " (对话轮次过多会使得顾客综合满意度下降。)",
+                "【提示】请输入“做菜”启动魔法锅，它会按所选定食材产生菜品。" " (对话轮次过多会使得顾客综合满意度下降。)",
             )
             msg = player(msg)
             if len(msg["content"]) == 0 or "[TERMINATE]" in msg["content"]:
@@ -213,7 +219,8 @@ def main() -> None:
     if args.load_checkpoint is not None:
         checkpoint = load_game_checkpoint(args.load_checkpoint)
         logger.debug(
-            "load checkpoint\n" + str(checkpoint.stage_per_night)
+            "load checkpoint\n"
+            + str(checkpoint.stage_per_night)
             + str(checkpoint.cur_plots),
         )
     else:
@@ -256,7 +263,10 @@ def main() -> None:
                 checkpoint.cur_plots,
             )
             if done_plot_idx is not None:
-                next_active_roles, active_plots = check_active_plot(plots, done_plot_idx)
+                next_active_roles, active_plots = check_active_plot(
+                    plots,
+                    done_plot_idx,
+                )
                 logger.debug("---active_plots:", active_plots)
                 checkpoint.cur_plots = active_plots
                 checkpoint.done_plots += [done_plot_idx]
@@ -312,8 +322,8 @@ if __name__ == "__main__":
     agentscope.init(model_configs=[TONGYI_CONFIG], logger_level="DEBUG")
     game_description = """
     这是一款模拟餐馆经营的文字冒险游戏，玩家扮演餐馆老板，通过与顾客互动来经营餐馆并解锁剧情。
-    游戏分为三个阶段：随意聊天，一对一互动以及邀请对话。
-    玩家需要根据顾客的喜好和需求来推荐菜品，通过顾客对用餐的满意度来获取好感度并解锁剧情。
+    游戏分为四个阶段：选择食材做菜，随意聊天，一对一互动以及邀请对话。
+    玩家需要根据顾客的喜好和需求来挑选食材做菜，通过顾客对用餐的满意度来获取好感度并解锁剧情。
     在游戏中，玩家需要经营餐厅、与顾客互动并决定邀请哪些顾客参与对话，以推动故事剧情的发展。
     通过与顾客的互动，玩家可以解锁剧情并发展餐馆的故事，体验不同的情节和结局。
     """
