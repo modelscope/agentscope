228bcdaaaf41a11f819de72d5176c5204259710d
zhijian.mzj <zhijian.mzj@alibaba-inc.com>
2024-02-01 09:03:02 +0800

    fix: use default avatar when wanx failed

--- examples/game/generate_image.py
+++ examples/game/generate_image.py
@@ -9,28 +9,32 @@ def generate_user_logo_file(desc, name, uuid_str):
     logo_path = get_user_dir(uuid_str)
     logo_file = os.path.join(logo_path, name + '.png')
     generate_logo_file(desc, logo_file)
-    return logo_file
+    return logo_file if os.path.exists(logo_file) else None
 
 def generate_logo_file(desc, logo_file):
 
+    from dashscope.common.error import InvalidTask
     dashscope.api_key = os.environ.get("DASHSCOPE_API_KEY") or dashscope.api_key
     assert dashscope.api_key
     
     prompt = LOGO_PROMPT.format(desc=desc)
-    rsp = dashscope.ImageSynthesis.call(
-        model='wanx-lite',
-        prompt=prompt,
-        n=1,
-        size='768*768')
+    try:
+        rsp = dashscope.ImageSynthesis.call(
+            model='wanx-lite',
+            prompt=prompt,
+            n=1,
+            size='768*768')
 
-    # save file to current directory
-    if rsp.status_code == HTTPStatus.OK:
-        # if os.path.exists(logo_file):
-        #     os.remove(logo_file)
-        for result in rsp.output.results:
-            with open(logo_file, 'wb+') as f:
-                f.write(requests.get(result.url).content)
+        # save file to current directory
+        if rsp.status_code == HTTPStatus.OK:
+            # if os.path.exists(logo_file):
+            #     os.remove(logo_file)
+            for result in rsp.output.results:
+                with open(logo_file, 'wb+') as f:
+                    f.write(requests.get(result.url).content)
 
-    else:
-        print('Failed, status_code: %s, code: %s, message: %s' %
-              (rsp.status_code, rsp.code, rsp.message))
+        else:
+            print('Failed, status_code: %s, code: %s, message: %s' %
+                (rsp.status_code, rsp.code, rsp.message))
+    except InvalidTask as e:
+        print(e)
