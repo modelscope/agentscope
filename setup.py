a0b450c1498e4046279d0d968e993c1d5f0edf41
DavdGao <gaodawei.gdw@alibaba-inc.com>
2024-03-12 17:42:32 +0800

    add package google-generativeai

--- setup.py
+++ setup.py
@@ -44,12 +44,13 @@ minimal_requires = [
     "tiktoken",
     "Pillow",
     "requests",
-    "openai>=1.3.0",
     "numpy",
     "Flask==3.0.0",
     "Flask-Cors==4.0.0",
     "Flask-SocketIO==5.3.6",
     "dashscope==1.14.1",
+    "openai>=1.3.0",
+    "google-generativeai>=0.4.0",
 ]
 
 distribute_requires = minimal_requires + rpc_requires
