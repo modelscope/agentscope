01ff3802c37115320abeccbb19e051c65efe56fa
weirui.kwr@alibaba-inc.com <weirui.kwr@alibaba-inc.com>
2024-03-14 21:09:42 +0800

    pass format

--- examples/distributed/user_proxy_agent.py
+++ examples/distributed/user_proxy_agent.py
@@ -9,16 +9,22 @@ from agentscope.agents import UserAgent
 class UserProxyAgent(UserAgent):
     """User proxy agent class"""
 
-    def reply(
+    def reply(  # type: ignore [override]
         self,
         x: dict = None,
         required_keys: Optional[Union[list[str], str]] = None,
     ) -> dict:
+        """
+        Reply with `self.speak(x)`
+        """
         if x is not None:
             self.speak(x)
         return super().reply(x, required_keys)
 
     def observe(self, x: Union[dict, Sequence[dict]]) -> None:
+        """
+        Observe with `self.speak(x)`
+        """
         if x is not None:
             self.speak(x)  # type: ignore[arg-type]
             self.memory.add(x)
