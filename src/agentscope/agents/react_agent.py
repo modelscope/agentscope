c224d4a7fd67f3367079ba068c5212255a830961
DavdGao <gaodawei.gdw@alibaba-inc.com>
2024-03-26 20:15:13 +0800

    Finish building react agent and example

--- src/agentscope/agents/react_agent.py
+++ src/agentscope/agents/react_agent.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
+"""An agent class that implements the ReAct algorithm. The agent will reason
+and act iteratively to solve problems. More details can be found in the paper
+https://arxiv.org/abs/2210.03629.
 """
 import json
-from typing import Sequence, Tuple, Literal, List
-
-from loguru import logger
+from typing import Tuple, List
 
 from agentscope.agents import AgentBase
 from agentscope.message import Msg
@@ -13,7 +13,6 @@ from agentscope.service import ServiceResponse, ServiceExecStatus
 
 
 DEFAULT_TOOL_PROMPT = """
-
 The following tool functions are available in the format of
 ```
 {{index}}. {{function name}}: {{function description}}
@@ -52,41 +51,39 @@ Taking using web_search function as an example, the response should be like this
 }}
 """  # noqa
 
-OBSERVE_HINT_PROMPT = """
-Determine if you've achieved your goals based on the above dialogue history and execution results. 
-
-You should respond in the following format, which can be loaded by `json.loads` in Python:
-{{
-    "thought": "Your thought and analysis",
-    "achieved": true/false 
-}} 
+FUNCTION_RESULT_TITLE_PROMPT = """Execution Results:
 """
 
-FUNCTION_RESULT_TITLE_PROMPT = """
-Execute Results:
-"""
-
-FUNCTION_RESULT_PROMPT = """
-{index}. {function_name}:
+FUNCTION_RESULT_PROMPT = """{index}. {function_name}:
     [EXECUTE STATUS]: {status}
     [EXECUTE RESULT]: {result}
 """
 
 
 def parse_func(response: ModelResponse) -> ModelResponse:
+    """Parsing the response into a dict object."""
     return ModelResponse(raw=json.loads(response.text))
 
 
-class ReactAgent(AgentBase):
+class ReActAgent(AgentBase):
+    """An agent class that implements the ReAct algorithm. More details refer
+    to https://arxiv.org/abs/2210.03629.
+
+    Note this is an example implementation of ReAct algorithm in AgentScope.
+    We follow the idea within the paper, but the detailed prompt engineering
+    maybe different. Developers are encouraged to modify the prompt to fit
+    their own needs.
+    """
+
     def __init__(
         self,
         name: str,
         model_config_name: str,
-        tools: Sequence[Tuple[str, dict]],
+        tools: List[Tuple],
         sys_prompt: str = "You're a helpful assistant.",
         max_iters: int = 10,
         verbose: bool = True,
-    ):
+    ) -> None:
         """Initialize the ReAct agent with the given name, model config name
         and tools.
 
@@ -98,7 +95,7 @@ class ReactAgent(AgentBase):
             model_config_name (`str`):
                 The name of the model config, which is used to load model from
                 configuration.
-            tools (`Sequence[Tuple[str, dict]]`):
+            tools (`List[Tuple]`):
                 A list of tuples, each containing the name of a tool and the
                 tool's description in JSON schema format.
             max_iters (`int`, defaults to `10`):
@@ -122,19 +119,24 @@ class ReactAgent(AgentBase):
         tools_prompt = DEFAULT_TOOL_PROMPT.format(function_prompt=func_prompt)
 
         if sys_prompt.endswith("\n"):
-            sys_prompt = sys_prompt + tools_prompt
+            self.sys_prompt = sys_prompt + tools_prompt
         else:
-            sys_prompt = sys_prompt + "\n" + tools_prompt
+            self.sys_prompt = sys_prompt + "\n" + tools_prompt
 
         # Put sys prompt into memory
-        self.memory.add(Msg("system", sys_prompt, role="system"))
+        self.memory.add(Msg("system", self.sys_prompt, role="system"))
 
     def reply(self, x: dict = None) -> dict:
+        """The reply function that achieves the ReAct algorithm.
+        The more details please refer to https://arxiv.org/abs/2210.03629"""
+
         if self.memory:
             self.memory.add(x)
 
         for _ in range(self.max_iters):
-            ######################## Step 1: Thought ########################
+            # Step 1: Thought
+
+            self.speak(f" ITER {_+1}, STEP 1: REASONING ".center(70, "#"))
 
             # Generate LLM response
             prompt = self.model.format(
@@ -151,7 +153,10 @@ class ReactAgent(AgentBase):
             # Record the response in memory
             msg_thought = Msg(self.name, res, role="assistant")
 
-            self.speak(msg_thought)
+            # To better display the response, we reformat it by json.dumps here
+            self.speak(
+                Msg(self.name, json.dumps(res, indent=4), role="assistant"),
+            )
 
             if self.memory:
                 self.memory.add(msg_thought)
@@ -160,7 +165,9 @@ class ReactAgent(AgentBase):
             if len(res.get("function", [])) == 0:
                 return msg_thought
 
-            ######################## Step 2: Action ##########################
+            # Step 2: Action
+
+            self.speak(f" ITER {_+1}, STEP 2: ACTION ".center(70, "#"))
 
             # Execute functions
             execute_results = []
@@ -170,9 +177,20 @@ class ReactAgent(AgentBase):
                 execute_results.append(func_res)
 
             # Prepare prompt for execution results
-            execute_results_prompt = "\n".join([FUNCTION_RESULT_PROMPT.format_map(res) for res in execute_results])
+            execute_results_prompt = "\n".join(
+                [
+                    FUNCTION_RESULT_PROMPT.format_map(res)
+                    for res in execute_results
+                ],
+            )
             # Add title
-            execute_results_prompt = FUNCTION_RESULT_TITLE_PROMPT + execute_results_prompt
+            execute_results_prompt = (
+                FUNCTION_RESULT_TITLE_PROMPT + execute_results_prompt
+            )
+
+            # Note: Observing the execution results and generate response are
+            # finished in the next loop. We just put the execution results
+            # into memory, and wait for the next loop to generate response.
 
             # Record execution results into memory as a message from the system
             msg_res = Msg(
@@ -184,18 +202,15 @@ class ReactAgent(AgentBase):
             if self.memory:
                 self.memory.add(msg_res)
 
-            ######################## Step 3: Observe ########################
-
-            # Generate LLM response or
-            prompt_obs = self.model.format(
-                Msg("system", self.sys_prompt, role="system"),
-                self.memory.get_memory(),
-            )
-
-
+        return Msg(
+            "system",
+            "The agent has reached the maximum iterations.",
+            role="system",
+        )
 
     def execute_func(self, index: int, func_call: dict) -> dict:
         """Execute the tool function and return the result.
+
         Args:
             index (`int`):
                 The index of the tool function.
@@ -209,7 +224,8 @@ class ReactAgent(AgentBase):
         func_name = func_call["name"]
         func_args = func_call["arguments"]
 
-        logger.info(f"Executing function {func_name} with arguments {func_args}")
+        self.speak(f">>> Executing function {func_name} ...")
+
         try:
             func_res = self.func_name_mapping[func_name](**func_args)
         except Exception as e:
@@ -217,6 +233,8 @@ class ReactAgent(AgentBase):
                 status=ServiceExecStatus.ERROR,
                 content=str(e),
             )
+
+        self.speak(">>> END ")
 
         status = (
             "SUCCESS"
@@ -232,38 +250,15 @@ class ReactAgent(AgentBase):
             "result": func_res.content,
         }
 
-    def _customize_print(
-        self,
-        content: str,
-        color: Literal["red", "blue", "green"] = "blue",
-    ) -> None:
-        """Print the content with customized color.
-        Args:
-            content (`str`):
-                The content to be printed.
-            color (`Literal["red", "blue", "green"]`):
-                The color of the printed content.
-        """
-        # TODO: This is only a temporary solution for customized print.
-        #  The printed content won't be recorded in logging files, and
-        #  it will be replaced by a better solution soon.
-        if self.verbose:
-            if color == "red":
-                print("\033[31m" + content + "\033[0m")
-            elif color == "green":
-                print("\033[32m" + content + "\033[0m")
-            elif color == "blue":
-                print("\033[34m" + content + "\033[0m")
-            else:
-                print(content)
-
     def prepare_funcs_prompt(self, tools: List[Tuple]) -> Tuple[str, dict]:
         """Convert function descriptions from json schema format to
         string prompt format.
+
         Args:
             tools (`List[Tuple]`):
                 The list of tool functions and their descriptions in JSON
                 schema format.
+
         Returns:
             `Tuple[str, dict]`:
                 The string prompt for the tool functions and a function name
