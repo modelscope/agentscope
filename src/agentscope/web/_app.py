ded463f71468219a278b240ffdade7ed9ab6a4dd
weirui.kwr@alibaba-inc.com <weirui.kwr@alibaba-inc.com>
2024-04-10 17:35:03 +0800

    roll back

--- src/agentscope/web/_app.py
+++ src/agentscope/web/_app.py
@@ -10,7 +10,6 @@ from flask_socketio import SocketIO
 app = Flask(__name__)
 socketio = SocketIO(app)
 CORS(app)  # This will enable CORS for all routes
-PORT = 5050
 
 
 PATH_SAVE = ""
@@ -43,7 +42,7 @@ def get_projects() -> Response:
 @app.route("/")
 def home() -> str:
     """Render the home page."""
-    return render_template("home.html", ip="127.0.0.1", prot=PORT)
+    return render_template("home.html")
 
 
 @app.route("/run/<run_dir>")
@@ -89,12 +88,6 @@ def run_detail(run_dir: str) -> str:
     return render_template("run.html", runInfo=logging_and_dialog)
 
 
-@app.route("/workstation")
-def workstation() -> str:
-    """Workstation static html"""
-    return render_template("workstation.html")
-
-
 @socketio.on("connect")
 def on_connect() -> None:
     """Execute when a client is connected."""
@@ -120,8 +113,6 @@ def init(
         raise FileNotFoundError(f"The path {path_save} does not exist.")
 
     PATH_SAVE = path_save
-    global PORT
-    PORT = port
     socketio.run(
         app,
         host=host,
@@ -129,7 +120,3 @@ def init(
         debug=debug,
         allow_unsafe_werkzeug=True,
     )
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=PORT)
