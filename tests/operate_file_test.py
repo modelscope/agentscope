cc82278b708c06b3b38dcf0996a7b6ac36271442
garyzhang99 <garyzhang99@163.com>
2024-04-12 15:47:28 +0800

    dir test

--- tests/operate_file_test.py
+++ tests/operate_file_test.py
@@ -15,6 +15,8 @@ from agentscope.service import (
     write_text_file,
     read_json_file,
     write_json_file,
+    show_directory,
+    get_current_directory,
 )
 from agentscope.service.service_status import ServiceExecStatus
 
@@ -62,10 +64,17 @@ class OperateFileTest(unittest.TestCase):
 
     def test_dir(self) -> None:
         """Execute dir test."""
+
+        is_success = get_current_directory().status
+        self.assertEqual(is_success, ServiceExecStatus.SUCCESS)
+
         is_success = create_directory(self.dir_name).status
         self.assertEqual(is_success, ServiceExecStatus.SUCCESS)
 
         is_success = move_directory(self.dir_name, self.moved_dir_name).status
+        self.assertEqual(is_success, ServiceExecStatus.SUCCESS)
+
+        is_success = show_directory(self.moved_dir_name).status
         self.assertEqual(is_success, ServiceExecStatus.SUCCESS)
 
         is_success = delete_directory(self.moved_dir_name).status
