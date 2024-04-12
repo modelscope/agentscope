48b7a7d1b652bd53177994666cd5e74a8bd4caf4
garyzhang99 <garyzhang99@163.com>
2024-04-12 17:35:55 +0800

    init shell command test

---
+++ tests/execute_shell_command_test.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+""" Python code execution test."""
+import unittest
+import sys
+
+from agentscope.service import execute_shell_command
+from agentscope.service import ServiceExecStatus
+
+
+class ExecuteShellCommandTest(unittest.TestCase):
+    """
+    Python code execution test.
+    """
+
+    def setUp(self) -> None:
+        """Init for ExecuteShellCommandTest."""
+
+        # Basic expression
+        self.arg0 = "touch tmp_a.text"
+
+        self.arg1 = "echo 'Helloworld' >> tmp_a.txt"
+
+        self.args2 = "cat tmp_a.txt"
+
+        self.args3 = "rm tmp_a.txt"
+
+    def test(self) -> None:
+        """test command"""
+        result = execute_shell_command(
+            command=self.arg0,
+        )
+        assert result.status == ServiceExecStatus.SUCCESS
+        assert result.content == "Success."
+
+        result = execute_shell_command(
+            command=self.arg1,
+        )
+        assert result.status == ServiceExecStatus.SUCCESS
+        assert result.content == "Success."
+
+        result = execute_shell_command(
+            command=self.arg2,
+        )
+        assert result.status == ServiceExecStatus.SUCCESS
+        assert result.content == "helloworld"
+
+        result = execute_shell_command(
+            command=self.arg3,
+        )
+        assert result.status == ServiceExecStatus.SUCCESS
+        assert result.content == "Success."
+
+        result = execute_shell_command(
+            command=self.arg3,
+        )
+        assert result.status == ServiceExecStatus.ERROR
+
+
+if __name__ == "__main__":
+    unittest.main()
