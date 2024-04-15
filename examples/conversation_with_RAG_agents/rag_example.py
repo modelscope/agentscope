7551c2f829e75829490a00ea84a46c9a2b772ac0
ZiTao-Li <zitao.l@alibaba-inc.com>
2024-04-15 15:08:36 +0800

    handle irrelevant question and simplify the setting first

--- examples/conversation_with_RAG_agents/rag_example.py
+++ examples/conversation_with_RAG_agents/rag_example.py
@@ -10,34 +10,33 @@ from rag_agents import LlamaIndexAgent
 
 import agentscope
 from agentscope.agents import UserAgent
-from agentscope.message import Msg
-from agentscope.agents import DialogAgent
+
+# from agentscope.message import Msg
+# from agentscope.agents import DialogAgent
 
 
 def main() -> None:
     """A RAG multi-agent demo"""
-    agentscope.init(
-        model_configs=[
-            {
-                "model_type": "dashscope_chat",
-                "config_name": "qwen_config",
-                "model_name": "qwen-max",
-                "api_key": f"{os.environ.get('DASHSCOPE_API_KEY')}",
-            },
-            {
-                "model_type": "dashscope_text_embedding",
-                "config_name": "qwen_emb_config",
-                "model_name": "text-embedding-v2",
-                "api_key": f"{os.environ.get('DASHSCOPE_API_KEY')}",
-            },
-        ],
-    )
+    with open("configs/model_config.json", "r", encoding="utf-8") as f:
+        model_configs = json.load(f)
+    # for internal API
+    for config in model_configs:
+        if config.get("model_type", "") == "post_api_chat":
+            # for gpt4 API
+            config["headers"]["Authorization"] = (
+                "Bearer " + f"{os.environ.get('HTTP_LLM_API_KEY')}"
+            )
+        else:
+            # for dashscope
+            config["api_key"] = f"{os.environ.get('DASHSCOPE_API_KEY')}"
+    agentscope.init(model_configs=model_configs)
 
-    with open("./agent_config.json", "r", encoding="utf-8") as f:
+    with open("configs/agent_config.json", "r", encoding="utf-8") as f:
         agent_configs = json.load(f)
     tutorial_agent = LlamaIndexAgent(**agent_configs[0]["args"])
-    code_explain_agent = LlamaIndexAgent(**agent_configs[1]["args"])
-    summarize_agent = DialogAgent(**agent_configs[2]["args"])
+    # TODO: testing version only with tutorial agent
+    # code_explain_agent = LlamaIndexAgent(**agent_configs[1]["args"])
+    # summarize_agent = DialogAgent(**agent_configs[2]["args"])
 
     user_agent = UserAgent()
     # start the conversation between user and assistant
@@ -46,18 +45,19 @@ def main() -> None:
         x.role = "user"  # to enforce dashscope requirement on roles
         if len(x["content"]) == 0 or str(x["content"]).startswith("exit"):
             break
-        tutorial_response = tutorial_agent(x)
-        code_explain = code_explain_agent(x)
-        msg = Msg(
-            name="user",
-            role="user",
-            content=tutorial_response["content"]
-            + "\n"
-            + code_explain["content"]
-            + "\n"
-            + x["content"],
-        )
-        summarize_agent(msg)
+        tutorial_agent(x)
+        # tutorial_response = tutorial_agent(x)
+        # code_explain = code_explain_agent(x)
+        # msg = Msg(
+        #     name="user",
+        #     role="user",
+        #     content=tutorial_response["content"]
+        #     + "\n"
+        #     + code_explain["content"]
+        #     + "\n"
+        #     + x["content"],
+        # )
+        # summarize_agent(msg)
 
 
 if __name__ == "__main__":
