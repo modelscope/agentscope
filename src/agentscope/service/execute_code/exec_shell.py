88d0cd06a7d43d40a4d91f5d5bca05d27fe7d624
garyzhang99 <garyzhang99@163.com>
2024-04-18 16:00:42 +0800

    precomit

--- src/agentscope/service/execute_code/exec_shell.py
+++ src/agentscope/service/execute_code/exec_shell.py
@@ -20,7 +20,7 @@ def execute_shell_command(command: str) -> ServiceResponse:
     Note:
         Use any bash/shell commands you want (e.g. find, grep, cat, ls),
         but note that :
-        1. interactive session commands (e.g. python, vim) or commands that 
+        1. interactive session commands (e.g. python, vim) or commands that
         change current state (e.g. cd that change the current directory)
         are NOT supported yet, so please do not invoke them.
         2. be VERY CAREFUL when using commands that will
