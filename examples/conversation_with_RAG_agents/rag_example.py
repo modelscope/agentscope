e55be4aba33d80dee55519577498feaf767adf31
艾渔 <feiwei@alibaba-inc.com>
2024-04-19 09:59:41 +0800

    Changes are made to improve the performance

--- examples/conversation_with_RAG_agents/rag_example.py
+++ examples/conversation_with_RAG_agents/rag_example.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-A simple example for conversation between user and
-an agent with RAG capability.
+An example for conversation between user and agents with RAG capability.
+One agent is a tutorial assistant, the other is a code explainer.
 """
 import json
 import os
@@ -12,7 +12,6 @@ from groupchat_utils import filter_agents
 import agentscope
 from agentscope.agents import UserAgent
 
-from agentscope.message import Msg
 from agentscope.agents import DialogAgent
 
 
@@ -37,19 +36,25 @@ def main() -> None:
     # define RAG-based agents for tutorial and code
     tutorial_agent = LlamaIndexAgent(**agent_configs[0]["args"])
     code_explain_agent = LlamaIndexAgent(**agent_configs[1]["args"])
-    agent_configs[2]["args"].pop("description")
     # define a guide agent
-    guide_agent = LlamaIndexAgent(**agent_configs[3]["args"])
+    agent_configs[2]["args"].pop("description")
+    guide_agent = DialogAgent(**agent_configs[2]["args"])
     rag_agents = [
         tutorial_agent,
         code_explain_agent,
-        guide_agent, # this is the new one
     ]
     rag_agent_names = [agent.name for agent in rag_agents]
-    helper_agents = rag_agents
 
     user_agent = UserAgent()
     while True:
+        """
+        The workflow is the following: 
+        1. user input a message, 
+        2. if it mentions one of the agents, then the agent will be called
+        3. otherwise, the guide agent will be decide which agent to call
+        4. the called agent will response to the user
+        5. repeat
+        """
         x = user_agent()
         x.role = "user"  # to enforce dashscope requirement on roles
         if len(x["content"]) == 0 or str(x["content"]).startswith("exit"):
@@ -57,13 +62,16 @@ def main() -> None:
         speak_list = filter_agents(x.get("content", ""), rag_agents)
         if len(speak_list) == 0:
             guide_response = guide_agent(x)
+            # Only one agent can be called in the current version,
+            # we may support multi-agent conversation later
             speak_list = filter_agents(guide_response.get("content", ""),
-                                       helper_agents)
-
+                                       rag_agents)
         agent_name_list = [agent.name for agent in speak_list]
         for agent_name, agent in zip(agent_name_list, speak_list):
             if agent_name in rag_agent_names:
                 agent(x)
 
+
 if __name__ == "__main__":
     main()
+
