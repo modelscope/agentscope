ec3d8c07f8b14fc93be01c88324159f2f3e7c9b5
ZiTao-Li <zitao.l@alibaba-inc.com>
2024-04-19 13:28:50 +0800

    add api assistant

--- examples/conversation_with_RAG_agents/rag_example.py
+++ examples/conversation_with_RAG_agents/rag_example.py
@@ -14,6 +14,18 @@ from agentscope.agents import UserAgent
 from agentscope.agents import DialogAgent
 
 
+def prepare_docstring_html(repo_path: str, html_dir: str) -> None:
+    """prepare docstring in html for API assistant"""
+    os.system(
+        f"sphinx-apidoc -f -o {repo_path}/docs/sphinx_doc/en/source "
+        f"{repo_path}/src/agentscope -t template",
+    )
+    os.system(
+        f"sphinx-build -b html  {repo_path}/docs/sphinx_doc/en/source "
+        f"{html_dir} -W --keep-going",
+    )
+
+
 def main() -> None:
     """A RAG multi-agent demo"""
     with open("configs/model_config.json", "r", encoding="utf-8") as f:
@@ -35,25 +47,32 @@ def main() -> None:
     # define RAG-based agents for tutorial and code
     tutorial_agent = LlamaIndexAgent(**agent_configs[0]["args"])
     code_explain_agent = LlamaIndexAgent(**agent_configs[1]["args"])
+    # prepare html for api agent
+    prepare_docstring_html(
+        agent_configs[2]["args"]["rag_config"]["repo_base"],
+        agent_configs[2]["args"]["rag_config"]["load_data"]["loader"][
+            "init_args"
+        ]["input_dir"],
+    )
+    api_agent = LlamaIndexAgent(**agent_configs[2]["args"])
     # define a guide agent
-    agent_configs[2]["args"].pop("description")
-    guide_agent = DialogAgent(**agent_configs[2]["args"])
+    agent_configs[3]["args"].pop("description")
+    guide_agent = DialogAgent(**agent_configs[3]["args"])
     rag_agents = [
         tutorial_agent,
         code_explain_agent,
+        api_agent,
     ]
     rag_agent_names = [agent.name for agent in rag_agents]
 
     user_agent = UserAgent()
     while True:
-        """
-        The workflow is the following: 
-        1. user input a message, 
-        2. if it mentions one of the agents, then the agent will be called
-        3. otherwise, the guide agent will be decide which agent to call
-        4. the called agent will response to the user
-        5. repeat
-        """
+        # The workflow is the following:
+        # 1. user input a message,
+        # 2. if it mentions one of the agents, then the agent will be called
+        # 3. otherwise, the guide agent will be decide which agent to call
+        # 4. the called agent will response to the user
+        # 5. repeat
         x = user_agent()
         x.role = "user"  # to enforce dashscope requirement on roles
         if len(x["content"]) == 0 or str(x["content"]).startswith("exit"):
@@ -63,8 +82,10 @@ def main() -> None:
             guide_response = guide_agent(x)
             # Only one agent can be called in the current version,
             # we may support multi-agent conversation later
-            speak_list = filter_agents(guide_response.get("content", ""),
-                                       rag_agents)
+            speak_list = filter_agents(
+                guide_response.get("content", ""),
+                rag_agents,
+            )
         agent_name_list = [agent.name for agent in speak_list]
         for agent_name, agent in zip(agent_name_list, speak_list):
             if agent_name in rag_agent_names:
@@ -73,4 +94,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
