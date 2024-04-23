21e1d8130884e45f54b43e718454f0708cb3f411
garyzhang99 <garyzhang99@163.com>
2024-04-23 19:42:50 +0800

    add doc string

--- src/agentscope/service/file/check.py
+++ src/agentscope/service/file/check.py
@@ -1,22 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Tools for checking files, such as linting and formatting.
+"""
 import subprocess
 
 from agentscope.service.service_response import ServiceResponse
 from agentscope.service.service_status import ServiceExecStatus
 
+
 def exec_py_linting(file_path: str) -> ServiceResponse:
     """
     Executes flake8 linting on the given .py file with specified checks and
     returns the linting result.
-    
+
     Args:
         file_path (`str`): The path to the Python file to lint.
-        
+
     Returns:
-        ServiceResponse: Contains either the output from the flake8 command as 
+        ServiceResponse: Contains either the output from the flake8 command as
         a string if successful, or an error message including the error type.
     """
-    command = f"flake8 --isolated --select=F821,F822,F831,E111,E112,E113,E999,E902 {file_path}"
-    
+    command = f"flake8 --isolated --select=F821,F822,F831,\
+        E111,E112,E113,E999,E902 {file_path}"
+
     try:
         result = subprocess.run(
             command,
@@ -28,7 +34,9 @@ def exec_py_linting(file_path: str) -> ServiceResponse:
         )
         return ServiceResponse(
             status=ServiceExecStatus.SUCCESS,
-            content=result.stdout.strip() if result.stdout else "No lint errors found.",
+            content=result.stdout.strip()
+            if result.stdout
+            else "No lint errors found.",
         )
     except subprocess.CalledProcessError as e:
         error_message = (
