533118e615cbc8b9aaed2ee7beda2ad77776f317
DavdGao <gaodawei.gdw@alibaba-inc.com>
2024-04-23 16:20:52 +0800

    [wip]

--- tests/service_factory_test.py
+++ tests/service_factory_test.py
@@ -22,16 +22,7 @@ class ServiceFactoryTest(unittest.TestCase):
 
     def setUp(self) -> None:
         """Init for ExampleTest."""
-
-    def test_bing_search(self) -> None:
-        """Test bing_search."""
-        # api_key is specified by developer, while question and num_results
-        # are specified by model
-        _, doc_dict = ServiceFactory.get(bing_search, api_key="xxx")
-        print(json.dumps(doc_dict, indent=4))
-        self.assertDictEqual(
-            doc_dict,
-            {
+        self.json_schema_bing_search1 = {
                 "type": "function",
                 "function": {
                     "name": "bing_search",
@@ -60,19 +51,9 @@ class ServiceFactoryTest(unittest.TestCase):
                         ],
                     },
                 },
-            },
-        )
+            }
 
-        # Set num_results by developer rather than model
-        _, doc_dict = ServiceFactory.get(
-            bing_search,
-            num_results=3,
-            api_key="xxx",
-        )
-
-        self.assertDictEqual(
-            doc_dict,
-            {
+        self.json_schema_bing_search2 = {
                 "type": "function",
                 "function": {
                     "name": "bing_search",
@@ -91,25 +72,9 @@ class ServiceFactoryTest(unittest.TestCase):
                         "required": ["question"],
                     },
                 },
-            },
-        )
+            }
 
-    def test_enum(self) -> None:
-        """Test enum in service factory."""
-
-        def func(  # type: ignore
-            a: str,
-            b,
-            c="test",
-            d: Literal[1, "abc", "d"] = 1,
-        ) -> None:
-            print(a, b, c, d)
-
-        _, doc_dict = ServiceFactory.get(func)
-
-        self.assertDictEqual(
-            doc_dict,
-            {
+        self.json_schema_func = {
                 "type": "function",
                 "function": {
                     "name": "func",
@@ -129,21 +94,9 @@ class ServiceFactoryTest(unittest.TestCase):
                         "required": ["a", "b"],
                     },
                 },
-            },
-        )
+            }
 
-    def test_exec_python_code(self) -> None:
-        """Test execute_python_code in service factory."""
-        _, doc_dict = ServiceFactory.get(
-            execute_python_code,
-            timeout=300,
-            use_docker=True,
-            maximum_memory_bytes=None,
-        )
-
-        self.assertDictEqual(
-            doc_dict,
-            {
+        self.json_schema_execute_python_code = {
                 "type": "function",
                 "function": {
                     "name": "execute_python_code",
@@ -161,23 +114,9 @@ class ServiceFactoryTest(unittest.TestCase):
                         "required": ["code"],
                     },
                 },
-            },
-        )
+            }
 
-    def test_retrieval(self) -> None:
-        """Test retrieval in service factory."""
-        _, doc_dict = ServiceFactory.get(
-            retrieve_from_list,
-            knowledge=[1, 2, 3],
-            score_func=lambda x, y: 1.0,
-            top_k=10,
-            embedding_model=10,
-            preserve_order=True,
-        )
-
-        self.assertDictEqual(
-            doc_dict,
-            {
+        self.json_schema_retrieve_from_list = {
                 "type": "function",
                 "function": {
                     "name": "retrieve_from_list",
@@ -194,25 +133,9 @@ class ServiceFactoryTest(unittest.TestCase):
                         ],
                     },
                 },
-            },
-        )
+            }
 
-    def test_sql_query(self) -> None:
-        """Test sql_query in service factory."""
-        _, doc_dict = ServiceFactory.get(
-            query_mysql,
-            database="test",
-            host="localhost",
-            user="root",
-            password="xxx",
-            port=3306,
-            allow_change_data=False,
-            maxcount_results=None,
-        )
-
-        self.assertDictEqual(
-            doc_dict,
-            {
+        self.json_schema_query_mysql = {
                 "type": "function",
                 "function": {
                     "name": "query_mysql",
@@ -230,25 +153,9 @@ class ServiceFactoryTest(unittest.TestCase):
                         ],
                     },
                 },
-            },
-        )
+            }
 
-    def test_summary(self) -> None:
-        """Test summarization in service factory."""
-        _, doc_dict = ServiceFactory.get(
-            summarization,
-            model=ModelWrapperBase("abc"),
-            system_prompt="",
-            summarization_prompt="",
-            max_return_token=-1,
-            token_limit_prompt="",
-        )
-
-        print(json.dumps(doc_dict, indent=4))
-
-        self.assertDictEqual(
-            doc_dict,
-            {
+        self.json_schema_summarization = {
                 "type": "function",
                 "function": {
                     "name": "summarization",
@@ -268,7 +175,148 @@ class ServiceFactoryTest(unittest.TestCase):
                         ],
                     },
                 },
-            },
+            }
+
+    def test_bing_search(self) -> None:
+        """Test bing_search."""
+        # api_key is specified by developer, while question and num_results
+        # are specified by model
+        _, doc_dict = ServiceFactory.get(bing_search, api_key="xxx")
+        print(json.dumps(doc_dict, indent=4))
+        self.assertDictEqual(
+            doc_dict,
+            self.json_schema_bing_search1
+        )
+
+        # Set num_results by developer rather than model
+        _, doc_dict = ServiceFactory.get(
+            bing_search,
+            num_results=3,
+            api_key="xxx",
+        )
+
+        self.assertDictEqual(
+            doc_dict,
+            self.json_schema_bing_search2
+        )
+
+    def test_enum(self) -> None:
+        """Test enum in service factory."""
+
+        def func(  # type: ignore
+            a: str,
+            b,
+            c="test",
+            d: Literal[1, "abc", "d"] = 1,
+        ) -> None:
+            print(a, b, c, d)
+
+        _, doc_dict = ServiceFactory.get(func)
+
+        self.assertDictEqual(
+            doc_dict,
+            self.json_schema_func
+        )
+
+    def test_exec_python_code(self) -> None:
+        """Test execute_python_code in service factory."""
+        _, doc_dict = ServiceFactory.get(
+            execute_python_code,
+            timeout=300,
+            use_docker=True,
+            maximum_memory_bytes=None,
+        )
+
+        self.assertDictEqual(
+            doc_dict,
+            self.json_schema_execute_python_code
+        )
+
+    def test_retrieval(self) -> None:
+        """Test retrieval in service factory."""
+        _, doc_dict = ServiceFactory.get(
+            retrieve_from_list,
+            knowledge=[1, 2, 3],
+            score_func=lambda x, y: 1.0,
+            top_k=10,
+            embedding_model=10,
+            preserve_order=True,
+        )
+
+        self.assertDictEqual(
+            doc_dict,
+            self.json_schema_retrieve_from_list
+        )
+
+    def test_sql_query(self) -> None:
+        """Test sql_query in service factory."""
+        _, doc_dict = ServiceFactory.get(
+            query_mysql,
+            database="test",
+            host="localhost",
+            user="root",
+            password="xxx",
+            port=3306,
+            allow_change_data=False,
+            maxcount_results=None,
+        )
+
+        self.assertDictEqual(
+            doc_dict,
+            self.json_schema_query_mysql
+        )
+
+    def test_summary(self) -> None:
+        """Test summarization in service factory."""
+        _, doc_dict = ServiceFactory.get(
+            summarization,
+            model=ModelWrapperBase("abc"),
+            system_prompt="",
+            summarization_prompt="",
+            max_return_token=-1,
+            token_limit_prompt="",
+        )
+
+        print(json.dumps(doc_dict, indent=4))
+
+        self.assertDictEqual(
+            doc_dict,
+            self.json_schema_summarization
+        )
+
+    def test_object_service_factory(self):
+        service_factory = ServiceFactory()
+
+        service_factory.add(bing_search, api_key="xxx")
+        service_factory.add(
+            execute_python_code,
+            timeout=300,
+            use_docker=True,
+            maximum_memory_bytes=None,
+        )
+
+        self.assertEqual(service_factory.tools_instruction,
+                         """## Tool Functions:
+The following tool functions are available in the format of
+```
+{index}. {function name}: {function description}
+{argument1 name} ({argument type}): {argument description}
+{argument2 name} ({argument type}): {argument description}
+...
+```
+
+1. bing_search: Search question in Bing Search API and return the searching results
+	question (string): The search query string.
+	num_results (number): The number of search results to return.
+2. execute_python_code: Execute a piece of python code.
+	code (string): The Python code to be executed.
+""")
+        self.assertDictEqual(
+            service_factory.json_schemas,
+            {
+                "bing_search": self.json_schema_bing_search1,
+                "execute_python_code": self.json_schema_execute_python_code
+            }
         )
 
 
