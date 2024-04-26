9290d8c5f019752c4744e4feee61962128fd3cb4
DavdGao <gaodawei.gdw@alibaba-inc.com>
2024-04-26 16:31:11 +0800

    fix bug

--- src/agentscope/parsers/__init__.py
+++ src/agentscope/parsers/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """Model response parser module."""
-from parser_base import ParserBase
-from json_object_parser import (
+from .parser_base import ParserBase
+from .json_object_parser import (
     MarkdownJsonObjectParser,
     MarkdownJsonDictParser,
 )
-from code_block_parser import MarkdownCodeBlockParser
-from tagged_content_parser import (
+from .code_block_parser import MarkdownCodeBlockParser
+from .tagged_content_parser import (
     TaggedContent,
     MultiTaggedContentParser,
 )
