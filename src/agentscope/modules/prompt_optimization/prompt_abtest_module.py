694ccbd95e877ed62cdb3489bbbb21206bf9d4d9
garyzhang99 <garyzhang99@163.com>
2024-05-30 14:17:17 +0800

    init all module and format style

--- src/agentscope/modules/prompt_optimization/prompt_abtest_module.py
+++ src/agentscope/modules/prompt_optimization/prompt_abtest_module.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
-from typing import Any, Union, List
-from .prompt_opt_method import PromptOptMethodBase
-
+"""The Abtest module to show how different system prompt performs"""
+from typing import Union, List
 from loguru import logger
 
 from agentscope.models import load_model_by_config_name
 from agentscope.message import Msg
 from agentscope.web.studio.utils import user_input
+from .prompt_opt_method import PromptOptMethodBase
 
 
 class PromptAbTestModule:
+    """The Abtest module to show how different system prompt performs"""
+
     def __init__(
         self,
         model_config_name: str,
@@ -35,24 +37,31 @@ class PromptAbTestModule:
                 )
         self.save_dir = save_dir
 
-    def show_optimized_prompts(self):
+    def show_optimized_prompts(self) -> None:
+        """Show all the optimzed prompts"""
         for index, opt_prompt in enumerate(self.ab_prompt_list, start=1):
             logger.chat(f"\n## Generated System Prompt of Method {index}\n")
             logger.chat(opt_prompt)
             logger.chat("===" * 10 + "\n")  # 打印一个空行以增加可读性
 
-    def infer_with_system_prompt(self, user_query, system_prompt):
+    def infer_with_system_prompt(
+        self,
+        user_query: str,
+        system_prompt: str,
+    ) -> str:
+        """Send query with system prompt"""
         prompt = self.model.format(
             [
                 Msg(name="system", content=system_prompt, role="system"),
                 Msg(name="user", content=user_query, role="user"),
-            ]
+            ],
         )
         response = self.model(prompt).text
 
         return response
 
-    def compare_query_results(self, queries: List[str]):
+    def compare_query_results(self, queries: List[str]) -> None:
+        """Show the query results under different system prompts"""
         for index, query in enumerate(queries, start=1):
             logger.chat(f"## Query {index}:\n")
             logger.chat(query)
