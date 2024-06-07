5cac2b2f3f7333cbf8eea0e9ff9cedceccc6e5ec
panxuchen.pxc <panxuchen.pxc@alibaba-inc.com>
2024-06-07 12:30:31 +0800

    fix code display

--- src/agentscope/web/studio/static/js/dashboard-detail-code.js
+++ src/agentscope/web/studio/static/js/dashboard-detail-code.js
@@ -4,20 +4,20 @@ let editorInstance = null;
 function initializeDashboardDetailCodePage(codeUrl) {
     initializeMonacoEditor();
 
-    fetch('/api/code?run_dir=' + codeUrl)
-        .then(response => {
+    fetch("/api/code?run_dir=" + codeUrl)
+        .then((response) => {
             if (!response.ok) {
-                throw new Error('Connection error, cannot load the web page.');
+                throw new Error("Connection error, cannot load the web page.");
             }
             return response.json();
         })
-        .then(data => {
+        .then((data) => {
             console.log("Get ", data);
             currentCode = data;
             constructCodeFileList(codeUrl, data);
         })
-        .catch(error => {
-            console.error('Error encountered while loading page: ', error);
+        .catch((error) => {
+            console.error("Error encountered while loading page: ", error);
         });
 }
 
@@ -26,41 +26,53 @@ function constructCodeFileList(codeUrl, code) {
 
     if (code !== null && code !== undefined) {
         if (Object.keys(code).length === 0) {
-            codeFileRows = ['<div class="content-placeholder">No code available</div>'];
+            codeFileRows = [
+                '<div class="content-placeholder">No code available</div>',
+            ];
         } else {
-            Object.keys(code)
-                .forEach(key => codeFileRows.push(`<li onclick="displayCode('${key}');return false;">${key}</li>`));
+            Object.keys(code).forEach((key) =>
+                codeFileRows.push(
+                    `<li onclick="displayCode('${key}');return false;">${key}</li>`
+                )
+            );
         }
     }
 
-    document.getElementById('code-list').innerHTML = codeFileRows.join('\n');
+    document.getElementById("code-list").innerHTML = codeFileRows.join("\n");
 }
 
 function initializeMonacoEditor() {
-    if (editorInstance === null) {
-        require.config({paths: {'vs': 'https://cdn.jsdelivr.net/npm/monaco-editor@latest/min/vs'}});
-        require(['vs/editor/editor.main'], function () {
-            editorInstance = monaco.editor.create(document.getElementById('code-editor'), {
-                language: 'python',
-                theme: 'vs-light',
+    require.config({
+        paths: {
+            vs: "https://cdn.jsdelivr.net/npm/monaco-editor@latest/min/vs",
+        },
+    });
+    require(["vs/editor/editor.main"], function () {
+        editorInstance = monaco.editor.create(
+            document.getElementById("code-editor"),
+            {
+                language: "python",
+                theme: "vs-light",
                 scrollBeyondLastLine: false,
                 readOnly: true,
-            });
-        }, function (error) {
-            console.error('Error encountered while loading monaco editor: ', error);
-        });
-    }
+            }
+        );
+    }, function (error) {
+        console.error("Error encountered while loading monaco editor: ", error);
+    });
 }
 
 function displayCode(codeFileName) {
-    document.getElementById('code-filename').innerHTML = codeFileName;
+    document.getElementById("code-filename").innerHTML = codeFileName;
 
     if (editorInstance) {
         console.log("Monaco editor instance is available, set value");
         editorInstance.setValue(currentCode[codeFileName]);
-
     } else {
-        console.log("Monaco editor instance is not available, set text content")
-        document.getElementById('code-editor').textContent = currentCode[codeFileName];
+        console.log(
+            "Monaco editor instance is not available, set text content"
+        );
+        document.getElementById("code-editor").textContent =
+            currentCode[codeFileName];
     }
 }
