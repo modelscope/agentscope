7a7eceaf443587d4585d20ca84f1235083c7931f
panxuchen.pxc <panxuchen.pxc@alibaba-inc.com>
2024-06-07 12:14:11 +0800

    jump to dashboard main

--- src/agentscope/web/studio/static/js/dashboard.js
+++ src/agentscope/web/studio/static/js/dashboard.js
@@ -25,12 +25,7 @@ function loadRunsPageInDashboardContent(pageUrl, javascriptUrl) {
                 initializeDashboardRunsPage();
             }
 
-            // Update the title bar of the dashboard page
-            let titleBar = document.getElementById("dashboard-titlebar");
-            titleBar.innerHTML =
-                "<span class='dashboard-titlebar-span unselectable-text'>Dashboard</span>";
             // Jump to specific run if url contains run_id
-            // TODO: cannot access this url
             const urlParams = new URLSearchParams(window.location.search);
             if (urlParams.has("run_id")) {
                 var loc = window.location;
@@ -92,12 +87,18 @@ function loadDetailPageInDashboardContent(pageUrl, javascriptUrl, runtimeInfo) {
             // Update the title bar of the dashboard page
             let titleBar = document.getElementById("dashboard-titlebar");
             // TODO: Add link to the dashboard page
-            titleBar.innerHTML =
-                "<span class='dashboard-titlebar-span unselectable-text' >Dashboard</span>" +
+            titleBar.innerHTML +=
                 '<svg class="dashboard-titlebar-svg" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path d="M718.16751787 491.2381952a38.36258987 38.36258987 0 0 0-4.898816-5.99763627L365.71163307 137.68349013c-14.9291008-14.9291008-39.1348224-14.9291008-54.0639232 0s-14.9291008 39.1348224 0 54.0639232l320.52456106 320.52565334-320.52565333 320.52456106c-14.9291008 14.9291008-14.9291008 39.1348224 0 54.0639232s39.1348224 14.9291008 54.0639232 0l347.5570688-347.5570688a38.08733867 38.08733867 0 0 0 11.1968256-27.03250773 38.141952 38.141952 0 0 0-6.29691733-21.0337792z"></path></svg>' +
-                "<span class='dashboard-titlebar-span unselectable-text'>Runtime ID: " +
+                "<span class='dashboard-subtitlebar-span unselectable-text'>Runtime ID: " +
                 runtimeInfo.run_id +
                 "</span>";
+            let mainTitle = document.getElementById("dashboard-main-title");
+            mainTitle.onclick = () => {
+                loadTabPage(
+                    "static/html/dashboard.html",
+                    "static/js/dashboard.js"
+                );
+            };
         })
         .catch((error) => {
             console.error("Error encountered while loading page: ", error);
