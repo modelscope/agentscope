4dd5249afcf9c030647f92bea80a3229a49a9852
panxuchen.pxc <panxuchen.pxc@alibaba-inc.com>
2024-07-29 13:44:17 +0800

    update python

--- setup.py
+++ setup.py
@@ -46,8 +46,6 @@ doc_requires = [
 test_requires = ["pytest", "pytest-cov", "pre-commit"]
 
 gradio_requires = [
-    "build==1.2.1",
-    "ffmpy==0.3.1",
     "gradio==4.19.1",
     "modelscope_studio==0.0.5",
 ]
