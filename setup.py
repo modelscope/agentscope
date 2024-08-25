801bcb389abf684a6b37ec3ebede01ed3160804f
chenyushuo <297086016@qq.com>
2024-08-25 17:41:07 +0800

    fix dependencies

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+                      '-DPython3_EXECUTABLE=' + sys.executable]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
