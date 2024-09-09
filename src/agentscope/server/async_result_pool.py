ae2ae9aa73971d3e7bc26d1e87700a60fc927445
panxuchen.pxc <panxuchen.pxc@alibaba-inc.com>
2024-09-09 19:46:54 +0800

    fix redis pool

--- src/agentscope/server/async_result_pool.py
+++ src/agentscope/server/async_result_pool.py
@@ -118,8 +118,10 @@ class RedisPool(AsyncResultPool):
         return self._get_object_id()
 
     def set(self, key: int, value: bytes) -> None:
-        self.pool.set(key, value, ex=self.max_timeout)
-        self.pool.rpush(RedisPool.TASK_QUEUE_PREFIX + str(key), key)
+        pipe = self.pool.pipeline()
+        pipe.set(key, value, ex=self.max_timeout)
+        pipe.rpush(RedisPool.TASK_QUEUE_PREFIX + str(key), key)
+        pipe.execute()
 
     def get(self, key: int) -> bytes:
         result = self.pool.get(key)
