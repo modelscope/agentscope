3be75be0a1ddc00b614871bc02820724f0333850
DavdGao <gaodawei.gdw@alibaba-inc.com>
2025-01-06 10:07:45 +0800

    Support explicit model wrapper registration (#504)

--- src/agentscope/models/__init__.py
+++ src/agentscope/models/__init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 """ Import modules in models package."""
-from typing import Type
-
-from loguru import logger
 
 from .model import ModelWrapperBase
 from .response import ModelResponse
@@ -42,6 +39,26 @@ from .yi_model import (
     YiChatWrapper,
 )
 
+_BUILD_IN_MODEL_WRAPPERS = [
+    "PostAPIChatWrapper",
+    "OpenAIChatWrapper",
+    "OpenAIDALLEWrapper",
+    "OpenAIEmbeddingWrapper",
+    "DashScopeChatWrapper",
+    "DashScopeImageSynthesisWrapper",
+    "DashScopeTextEmbeddingWrapper",
+    "DashScopeMultiModalWrapper",
+    "OllamaChatWrapper",
+    "OllamaEmbeddingWrapper",
+    "OllamaGenerationWrapper",
+    "GeminiChatWrapper",
+    "GeminiEmbeddingWrapper",
+    "ZhipuAIChatWrapper",
+    "ZhipuAIEmbeddingWrapper",
+    "LiteLLMChatWrapper",
+    "YiChatWrapper",
+]
+
 __all__ = [
     "ModelWrapperBase",
     "ModelResponse",
@@ -65,22 +82,3 @@ __all__ = [
     "LiteLLMChatWrapper",
     "YiChatWrapper",
 ]
-
-
-def _get_model_wrapper(model_type: str) -> Type[ModelWrapperBase]:
-    """Get the specific type of model wrapper
-
-    Args:
-        model_type (`str`): The model type name.
-
-    Returns:
-        `Type[ModelWrapperBase]`: The corresponding model wrapper class.
-    """
-    wrapper = ModelWrapperBase.get_wrapper(model_type=model_type)
-    if wrapper is None:
-        logger.warning(
-            f"Unsupported model_type [{model_type}],"
-            "use PostApiModelWrapper instead.",
-        )
-        return PostAPIModelWrapperBase
-    return wrapper
