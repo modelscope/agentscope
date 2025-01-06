61305a6b1c4b8b3b5f86488c83b79188e5addf0e
DavdGao <gaodawei.gdw@alibaba-inc.com>
2025-01-06 18:14:08 +0800

    debug

--- docs/tutorial/en/build.sh
+++ docs/tutorial/en/build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 echo "Start ..."
-if [ -n "$DASHSCOPE_API_KEY" ]; then
-    echo "DASHSCOPE_API_KEY is set and not empty"
-    echo "DASHSCOPE_API_KEY=$DASHSCOPE_API_KEY"
-else
-    echo "DASHSCOPE_API_KEY is not set or is empty"
-fi
+python -c "import os; print('!!!' + os.environ['DASHSCOPE_API_KEY'] + '+++')"
 echo "Done"
 
 sphinx-build -M html source build
