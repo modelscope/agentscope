1071e51d3b5148fcdc569cce6a64e8417a85db19
DavdGao <gaodawei.gdw@alibaba-inc.com>
2025-01-06 18:33:39 +0800

    debug

--- docs/tutorial/en/build.sh
+++ docs/tutorial/en/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "Start ..."
-python -c "import os; print('!!!' + os.environ['DASHSCOPE_API_KEY'] + '+++')"
-echo "Done"
-
 if [ -z "$DASHSCOPE_API_KEY" ]; then
   echo "DASHSCOPE_API_KEY is not set" > debug.txt
 else
