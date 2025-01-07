d8d5d2990a530d8073c353f4a20ee8b0ee428610
DavdGao <gaodawei.gdw@alibaba-inc.com>
2025-01-07 12:33:43 +0800

    Update the tested version

--- docs/tutorial/en/build.sh
+++ docs/tutorial/en/build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-if [ -z "$DASHSCOPE_API_KEY" ]; then
-  echo "DASHSCOPE_API_KEY is not set" > debug.txt
-else
-  echo "DASHSCOPE_API_KEY is set" > debug.txt
-fi
-
 sphinx-build -M html source build
