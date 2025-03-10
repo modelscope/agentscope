5b560a092ac740f10a6b7af9460abb8e462b6064
ZiTao-Li <ZiTao-Li@users.noreply.github.com>
2025-03-10 03:42:54 +0000

    deploy: c029b31777dcc27706207a11526f5337473844b2

---
+++ zh_CN/_downloads/d8213dcb04984a454132b528d3f88c35/web_browser.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+.. _web-browser-control:
+
+浏览器控制
+====================
+
+本节重定向到
+`conversation_with_web_browser_agent/README.md
+<https://github.com/modelscope/agentscope/tree/main/examples/conversation_with_web_browser_agent>`_。
+"""
