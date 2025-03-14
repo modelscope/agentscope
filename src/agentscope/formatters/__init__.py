e8b7224f0dd565de6915cc05bd3724e7ff0be348
大玮 <gaodawei.gdw@alibaba-inc.com>
2025-03-14 19:23:08 +0800

    Fix typo

--- src/agentscope/formatters/__init__.py
+++ src/agentscope/formatters/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """The formatter modules for different models."""
-from .common_formatter import CommonFormatter
 from .formatter_base import FormatterBase
+from .common_formatter import CommonFormatter
 from .anthropic_formatter import AnthropicFormatter
 from .gemini_formatter import GeminiFormatter
 from .openai_formatter import OpenAIFormatter
