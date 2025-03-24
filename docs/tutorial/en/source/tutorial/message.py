134e2f663314fd277bab6cd93a791f2c82a9c0ea
大玮 <gaodawei.gdw@alibaba-inc.com>
2025-03-24 11:49:56 +0800

    Update the tutorial

--- docs/tutorial/en/source/tutorial/message.py
+++ docs/tutorial/en/source/tutorial/message.py
@@ -35,6 +35,63 @@ print(f'The role of the sender: "{msg.role}"')
 print(f'The content of the message: "{msg.content}"')
 
 # %%
+# Multimodal Message
+# -------------------------
+# The `content` field also supports multimodal content, such as text, image,
+# audio, and video.
+# AgentScope uses the concept of block to represent the content of a message.
+# A block is a dictionary containing the type and content of the message,
+# and the specific supported types are as follows:
+#
+# .. list-table::
+#     :header-rows: 1
+#
+#     * - Class
+#       - Description
+#       - Example
+#     * - TextBlock
+#       - Text block
+#       - `TextBlock(type="text", text="你好")`
+#     * - ImageBlock
+#       - Image block
+#       - `ImageBlock(type="image", url="https://example.com/image.jpg")`
+#     * - AudioBlock
+#       - Audio block
+#       - `AudioBlock(type="audio", url="https://example.com/audio.mp3")`
+#     * - VideoBlock
+#       - Video block
+#       - `VideoBlock(type="video", url="https://example.com/video.mp4")`
+#     * - FileBlock
+#       - File block
+#       - `FileBlock(type="file", url="https://example.com/file.zip")`
+#
+# The usage is as follows:
+
+from agentscope.message import (
+    TextBlock,
+    ImageBlock,
+    AudioBlock,
+)
+
+msg_image = Msg(
+    name="Stank",
+    role="user",
+    content=[
+        TextBlock(type="text", text="Describe this image for me."),
+        ImageBlock(type="image", url="https://example.com/image.jpg"),
+    ],
+)
+
+msg_audio = Msg(
+    name="Stank",
+    role="user",
+    content=[
+        TextBlock(type="text", text="Listen to this audio."),
+        AudioBlock(type="audio", url="https://example.com/audio.mp3"),
+    ],
+)
+
+# %%
 # Serialize
 # ----------------
 # Message can be serialized to a string in JSON format.
