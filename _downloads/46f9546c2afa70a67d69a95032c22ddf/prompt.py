fedc0f39ac86ca0bb22a573cf3387022ee937005
DavdGao <DavdGao@users.noreply.github.com>
2025-03-24 06:15:13 +0000

    deploy: 678a509c6a5ba2928ed94c4360b3586a33e9abf0

--- _downloads/46f9546c2afa70a67d69a95032c22ddf/prompt.py
+++ _downloads/46f9546c2afa70a67d69a95032c22ddf/prompt.py
@@ -2,40 +2,65 @@
 """
 .. _prompt-engineering:
 
-Prompt Engineering
+Prompt Formatting
 ================================
 
-The prompt engineering is a crucial part of LLM-empowered applications,
-especially for the multi-agent ones.
-However, most API providers focus on the chatting scenario, where a user and
-an assistant speak alternately.
+AgentScope supports developers to build prompt that fits different model APIs
+by providing a set of built-in strategies for both chat and multi-agent
+scenarios.
 
-To support multi-agent applications, AgentScope builds different prompt
-strategies to convert a list of `Msg` objects to the required format.
+Specifically, AgentScope supports both model-specific and model-agnostic
+formatting.
 
-.. note:: There is no **one-size-fits-all** solution for prompt crafting.
+.. tip:: **Chat scenario** refers to the conversation between a user and an
+ assistant, while **multi-agent scenario** involves multiple agents with
+ different names (though their roles are all "assistant").
+
+.. note:: Currently, most LLM API providers only support chat scenario. For
+ example, only two roles (user and assistant) are involved in the conversation
+ and sometimes they must speak alternatively.
+
+.. note:: There is no **one-size-fits-all** solution for prompt formatting.
  The goal of built-in strategies is to **enable beginners to smoothly invoke
- the model API, rather than achieve the best performance**.
+ the model API, rather than achieving the best performance**.
  For advanced users, we highly recommend developers to customize prompts
  according to their needs and model API requirements.
 
-Using Built-in Strategy
+Model-Agnostic Formatting
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-The built-in prompt strategies are implemented in the `format` method of the
-model objects. Taking DashScope Chat API as an example:
+When you want your application to work with different model APIs
+simultaneously, the model-agnostic formatting is a good idea.
+
+AgentScope achieves model-agnostic formatting by supporting to load the model
+from configuration, and presets a collection of built-in formatting strategies
+for different model APIs and scenarios (chat or multi-agent) in the
+model wrapper class.
+
+You can directly use the `format` method of the model object to format the
+input messages without knowing the details of the model API. Taking DashScope
+Chat API as an example:
 
 """
+from typing import Union, Optional
 
-from agentscope.models import DashScopeChatWrapper
+from agentscope.agents import AgentBase
 from agentscope.message import Msg
+from agentscope.manager import ModelManager
+import agentscope
 import json
 
-
-model = DashScopeChatWrapper(
-    config_name="_",
-    model_name="qwen-max",
+# Load the model configuration
+agentscope.init(
+    model_configs={
+        "config_name": "my_qwen",
+        "model_type": "dashscope_chat",
+        "model_name": "qwen-max",
+    },
 )
+
+# Get the model object from model manager
+model = ModelManager.get_instance().get_model_by_config_name("my_qwen")
 
 # `Msg` objects or a list of `Msg` objects can be passed to the `format` method
 prompt = model.format(
@@ -44,6 +69,7 @@ prompt = model.format(
         Msg("assistant", "Hi!", "assistant"),
         Msg("user", "Nice to meet you!", "user"),
     ],
+    multi_agent_mode=False,
 )
 
 print(json.dumps(prompt, indent=4, ensure_ascii=False))
@@ -57,82 +83,162 @@ response = model(prompt)
 print(response.text)
 
 # %%
-# Non-Vision Models
+# Also, you can use format the messages in the multi-agent scenario by
+# setting `multi_agent_mode=True`.
+
+prompt = model.format(
+    Msg("system", "You're a helpful assistant named Alice.", "system"),
+    [
+        Msg("Alice", "Hi!", "assistant"),
+        Msg("Bob", "Nice to meet you!", "assistant"),
+    ],
+    multi_agent_mode=True,
+)
+
+print(json.dumps(prompt, indent=4, ensure_ascii=False))
+
+# %%
+# Within the agent, the model-agnostic formatting is achieved as follows:
+
+
+class MyAgent(AgentBase):
+    def __init__(self, name: str, model_config_name: str, **kwargs) -> None:
+        super().__init__(name=name, model_config_name=model_config_name)
+
+        # ...
+
+    def reply(self, x: Optional[Union[Msg, list[Msg]]] = None) -> Msg:
+        # ...
+
+        # Format the messages without knowing the model API
+        prompt = self.model.format(
+            Msg("system", "{your system prompt}", "system"),
+            self.memory.get_memory(),
+            multi_agent_mode=True,
+        )
+        response = self.model(prompt)
+
+        # ...
+        return Msg(self.name, response.text, role="assistant")
+
+
+# %%
+# .. tip:: All the formatting strategies are implemented under
+# `agentscope.formatter` module. The model wrapper decides which strategy to
+# use based on the model name.
+
+# %%
+# Model-Specific Formatting
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
-# In the following table, we list the built-in prompt strategies, as well as
-# the prefix of supported LLMs.
-#
-# Taking the following messages as an example:
-#
-# .. code-block:: python
-#
-#     Msg("system", "You're a helpful assistant named Alice.", "system"),
-#     Msg("Alice", "Hi!", "assistant"),
-#     Msg("Bob", "Nice to meet you!", "user")
-#
-#
-# .. list-table::
-#     :header-rows: 1
-#
-#     * - LLMs
-#       - `model_name`
-#       - Constructed Prompt
-#     * - OpenAI LLMs
-#       - `gpt-`
-#       - .. code-block:: python
-#
-#             [
-#                 {
-#                     "role": "system",
-#                     "name": "system",
-#                     "content": "You're a helpful assistant named Alice."
-#                 },
-#                 {
-#                     "role": "user",
-#                     "name": "Alice",
-#                     "content": "Hi!"
-#                 },
-#                 {
-#                     "role": "user",
-#                     "name": "Bob",
-#                     "content": "Nice to meet you!"
-#                 }
-#             ]
-#     * - Gemini LLMs
-#       - `gemini-`
-#       - .. code-block:: python
-#
-#             [
-#                 {
-#                     "role": "user",
-#                     "parts": [
-#                         "You're a helpful assistant named Alice.\\n## Conversation History\\nAlice: Hi!\\nBob: Nice to meet you!"
-#                     ]
-#                 }
-#             ]
-#     * - All other LLMs
-#
-#         (e.g. DashScope, ZhipuAI ...)
-#       -
-#       - .. code-block:: python
-#
-#             [
-#                 {
-#                     "role": "system",
-#                     "content": "You're a helpful assistant named Alice."
-#                 },
-#                 {
-#                     "role": "user",
-#                     "content": "## Conversation History\\nAlice: Hi!\\nBob: Nice to meet you!"
-#                 }
-#             ]
-#
-# .. tip:: Considering some API libraries can support different LLMs (such as OpenAI Python library), AgentScope uses the `model_name` field to distinguish different models and decides the used strategy.
+# The `agentscope.formatter` module implements the built-in formatting
+# strategies for different model APIs and scenarios. They provide
+# `format_chat` and `format_multi_agent` methods, as well as a `format_auto`
+# function that automatically selects the appropriate method based on the
+# input messages.
+
+from agentscope.formatters import OpenAIFormatter
+
+multi_agent_messages = [
+    Msg("system", "You're a helpful assistant named Alice.", "system"),
+    Msg("Alice", "Hi!", "assistant"),
+    Msg("Bob", "Nice to meet you!", "assistant"),
+    Msg("Charlie", "Nice to meet you, too!", "user"),
+]
+
+chat_messages = [
+    Msg("system", "You're a helpful assistant named Alice.", "system"),
+    Msg("Bob", "Nice to meet you!", "user"),
+    Msg("Alice", "Hi! How can I help you?", "assistant"),
+]
+
+
+# %%
+# Multi-agent scenario:
+
+formatted_multi_agent = OpenAIFormatter.format_multi_agent(
+    multi_agent_messages,
+)
+print(json.dumps(formatted_multi_agent, indent=4, ensure_ascii=False))
+
+# %%
+# Chat scenario:
+
+formatted_chat = OpenAIFormatter.format_chat(
+    chat_messages,
+)
+print(json.dumps(formatted_chat, indent=4, ensure_ascii=False))
+
+# %%
+# Auto formatting when only two entities are involved:
+
+formatted_auto_chat = OpenAIFormatter.format_auto(
+    chat_messages,
+)
+print(json.dumps(formatted_auto_chat, indent=4, ensure_ascii=False))
+
+# %%
+# Auto formatting when more than two entities (multi-agent) are involved:
+
+formatted_auto_multi_agent = OpenAIFormatter.format_auto(
+    multi_agent_messages,
+)
+print(json.dumps(formatted_auto_multi_agent, indent=4, ensure_ascii=False))
+
+# %%
+# The available formatter classes are:
+
+from agentscope.formatters import (
+    CommonFormatter,
+    AnthropicFormatter,
+    OpenAIFormatter,
+    GeminiFormatter,
+    DashScopeFormatter,
+)
+
+# %%
+# The `CommonFormatter` is a basic formatter for common chat LLMs, such as
+# ZhipuAI API, Yi API, ollama, LiteLLM, etc.
 #
 # Vision Models
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 #
-# For vision models, AgentScope currently supports OpenAI vision models and
-# Dashscope multi modal API.
-# The more supported APIs will be added in the future.
+# For vision models, AgentScope currently supports OpenAI, DashScope and
+# Anthropic vision models.
+
+from agentscope.message import TextBlock, ImageBlock
+
+# we create a fake image locally
+with open("./image.jpg", "w") as f:
+    f.write("fake image")
+
+multi_modal_messages = [
+    Msg("system", "You're a helpful assistant named Alice.", "system"),
+    Msg(
+        "Alice",
+        [
+            TextBlock(type="text", text="Help me to describe the two images?"),
+            ImageBlock(type="image", url="https://example.com/image.jpg"),
+            ImageBlock(type="image", url="./image.jpg"),
+        ],
+        "user",
+    ),
+    Msg("Bob", "Sure!", "assistant"),
+]
+
+# %%
+print("OpenAI prompt:")
+openai_prompt = OpenAIFormatter.format_chat(multi_modal_messages)
+print(json.dumps(openai_prompt, indent=4, ensure_ascii=False))
+
+# %%
+#
+print("\nDashscope prompt:")
+dashscope_prompt = DashScopeFormatter.format_chat(multi_modal_messages)
+print(json.dumps(dashscope_prompt, indent=4, ensure_ascii=False))
+
+# %%
+#
+print("\nAnthropic prompt:")
+anthropic_prompt = AnthropicFormatter.format_chat(multi_modal_messages)
+print(json.dumps(anthropic_prompt, indent=4, ensure_ascii=False))
--- _downloads/ede26fc984fd5f12a4ede3580d57b877/tool.py
+++ _downloads/ede26fc984fd5f12a4ede3580d57b877/tool.py
@@ -5,22 +5,35 @@
 Tools
 ====================
 
-In this tutorial we show how to use the built-in tools in AgentScope and
-how to create custom tools.
+Background
+--------------------
+
+There are two ways to call tools in LLM-empowered multi-agent applications.
+
+.. image:: https://img.alicdn.com/imgextra/i3/O1CN01iizvjY1UjKCE3q5FR_!!6000000002553-0-tps-1830-792.jpg
+   :align: center
+   :alt: Two ways for tool calling
+   :width: 80%
+
+- Prompt-based tool calling: Developers introduce tools in the prompt and extract tool calls from the LLM response.
+- API-based tool calling: Developers provide tools description in JSON schema format. The LLM API will directly return the tool calls in their specific format.
+
+AgentScope supports both ways. In this tutorial, we will introduce how to use
+the built-in tools and how to create custom tools.
 """
 import json
 
 import agentscope
 from agentscope.message import Msg
+from agentscope.models import DashScopeChatWrapper
 
 # %%
 # Using Built-in Tools
 # --------------------------
-# AgentScope provides a `ServiceToolkit` module that supports
+# AgentScope provides a `ServiceToolkit` module that supports to
 #
-# - tool introduction generation,
-# - a default call format,
-# - response parsing, tools calling and agent-oriented error handling.
+# - parse tools into JSON schemas automatically
+# - check arguments and call functions
 #
 # Before using `ServiceToolkit`, we can take a look at the available tools in
 # the `agentscope.service` module.
@@ -32,6 +45,8 @@ get_help()
 # %%
 # All above functions are implemented as Python functions.
 # They can be registered to the `ServiceToolkit` by calling the `add` method.
+# The `ServiceToolkit` will parse the tool functions into JSON schema
+# automatically.
 
 from agentscope.service import ServiceToolkit
 from agentscope.service import bing_search, execute_shell_command
@@ -39,6 +54,7 @@ from agentscope.service import bing_search, execute_shell_command
 toolkit = ServiceToolkit()
 toolkit.add(execute_shell_command)
 
+# %%
 # Note some parameters of the tool functions (e.g. api_key) should be handled
 # by developers.
 # You can directly pass these parameters as keyword arguments in the add
@@ -48,6 +64,167 @@ toolkit.add(bing_search, api_key="xxx")
 
 print("The tools instruction:")
 print(toolkit.tools_instruction)
+
+# %%
+# The built-in default calling format:
+
+print(toolkit.tools_calling_format)
+
+# %%
+# The JSON Schema description of the tool functions:
+
+print(json.dumps(toolkit.json_schemas, indent=2))
+
+# %%
+# Prompt-based Tool Calling
+# --------------------------
+# In prompt-based tool calling, developers need to
+# - introduce the tools and call format in prompt
+# - parse and extract the tool calls from the LLM response.
+#
+# You can use the parsers in :ref:`structured-output` section to parse the LLM
+# response and extract the tool calls.
+# The tool call format of `ServiceToolkit` is as follows:
+
+from agentscope.message import ToolUseBlock
+
+tool_call = ToolUseBlock(
+    type="tool_use",
+    id="xxx",
+    name="bing_search",
+    input={"query": "AgentScope"},
+)
+
+# %%
+# After assembling the `ServiceToolkit`, you can integrate it into agent.
+#
+# In AgentScope, we provide a `ReActAgent` to handle the tool usage, you can
+# directly pass the `ServiceToolkit` object into this agent.
+# Refer to :ref:`builtin-agent` for implementation details of this agent.
+#
+# .. note:: `ReActAgent` constructs the prompt and parses the tools locally,
+#  rather than through the tools API provided by the model API. For using the
+#  tools API, please refer to :ref:`tools-api`.
+
+from agentscope.agents import ReActAgent
+
+agentscope.init(
+    model_configs={
+        "config_name": "my-qwen-max",
+        "model_type": "dashscope_chat",
+        "model_name": "qwen-max",
+    },
+)
+
+agent = ReActAgent(
+    name="Friday",
+    model_config_name="my-qwen-max",
+    service_toolkit=toolkit,
+    sys_prompt="You're a helpful assistant named Friday.",
+)
+
+msg_task = Msg(
+    "user",
+    "Help me to calculate 1615114134*4343434343",
+    "user",
+)
+
+res = agent(msg_task)
+
+# %%
+# API-based Tool Calling
+# --------------------------
+#
+# In API-based tool calling, developers only need to prepare the tools
+# description in JSON schema format. However, different APIs differ in
+# - the format of the tool description, and
+# - how to construct the prompt with tool calls and execution results.
+#
+# .. image:: https://img.alicdn.com/imgextra/i4/O1CN01HIHrnw21LrDxzrB4a_!!6000000006969-0-tps-1920-1080.jpg
+#    :align: center
+#    :alt: API-based tool calling
+#    :width: 100%
+#
+# The above figure takes OpenAI as an example to show how API-based tool
+# calling works in AgentScope. We block API-specific requirements by
+# `agentscope.formatter` and `ModelResponse` modules. All developers need to
+# know is
+#
+# 1. `ServiceToolkit` will parse the tool functions into standard JSON schema automatically
+# 2. `Formatter` class will transform the JSON schemas and messages into the required format
+# 3. The tool calls are all unified into the same format (`ToolUseBlock`) within `ModelResponse`
+#
+# .. tip:: A new agent class `ReActAgentV2` is added for API-based tools calling!
+#
+# .. note:: Currently, only the `format_chat` method supports tools API. The
+#  `format_multi_agent` method will be supported in the future.
+#
+# .. note:: API-based tool calling does not support streaming return yet, and
+#  the related functionality is under development.
+#
+# Here we take DashScope as an example to show how to use the tools API.
+
+from agentscope.formatters import DashScopeFormatter
+from agentscope.message import TextBlock, ToolUseBlock, ToolResultBlock
+
+model = DashScopeChatWrapper(
+    config_name="_",
+    model_name="qwen-max",
+)
+
+# %%
+# Step 3 -> 4 in the figure, formating messages and JSON schemas:
+msgs = [
+    Msg("user", "Help me to execute shell cmd 'whoami'", "user"),
+]
+
+formatted_msgs = DashScopeFormatter.format_chat(msgs)
+formatted_schemas = DashScopeFormatter.format_tools_json_schemas(
+    toolkit.json_schemas,
+)
+print(json.dumps(formatted_msgs, indent=4, ensure_ascii=False))
+
+# %%
+# Step 5 -> 6 -> 7 in the figure, getting the model response:
+
+response = model(formatted_msgs, tools=formatted_schemas)
+print("tool_calls:", json.dumps(response.tool_calls, indent=4))
+
+# %%
+# Step 8, creating a new message with the tool calls:
+
+# Create a new msg with the tool calls
+content = []
+if response.text:
+    content.append(TextBlock(type="text", text=response.text))
+if response.tool_calls:
+    content.extend(response.tool_calls)
+msgs.append(Msg("assistant", content, "assistant", echo=True))
+
+# execute the tool calls
+msg_execution = toolkit.parse_and_call_func(
+    response.tool_calls,
+    tools_api_mode=True,  # Must be ture for tools API
+)
+
+# %%
+# Step 9, adding the execution results to the message list:
+
+msgs.append(msg_execution)
+
+# %%
+# Now, let's try to format the new message list with tool calls and result
+# again!
+
+formatted_msgs = DashScopeFormatter.format_chat(msgs)
+print(json.dumps(formatted_msgs, indent=4, ensure_ascii=False))
+
+
+# %%
+# Up to now, we have already finished the API-based tool calling process.
+# The whole process refers to the implementation of
+# `agentscope.agents.ReActAgentV2` class. You can also directly use this
+# agent.
 
 # %%
 # Using MCP with ServiceToolkit
@@ -67,53 +244,10 @@ configs = {
     },
 }
 
+# %%
 # Add MCP server configurations to the ServiceToolkit
 # `toolkit.add_mcp_servers(server_configs=configs)`
-
-# %%
-# The built-in default calling format:
-
-print(toolkit.tools_calling_format)
-
-# %%
-# The JSON Schema description of the tool functions:
-
-print(json.dumps(toolkit.json_schemas, indent=2))
-
-# %%
-
-# %%
-# After assembling the `ServiceToolkit`, you can integrate it into agent.
-# In AgentScope, we provide a `ReActAgent` to handle the tool usage, you can
-# directly pass the `ServiceToolkit` object into this agent.
-# Refer to [] for implementation details of this agent.
-
-from agentscope.agents import ReActAgent
-
-agentscope.init(
-    model_configs={
-        "config_name": "my-qwen-max",
-        "model_type": "dashscope_chat",
-        "model_name": "qwen-max",
-    },
-)
-
-agent = ReActAgent(
-    name="Friday",
-    model_config_name="my-qwen-max",
-    service_toolkit=toolkit,
-    sys_prompt="You're a helpful assistant named Friday.",
-)
-
-msg_task = Msg("user", "Help me to calculate 1615114134*4343434343", "user")
-
-res = agent(msg_task)
-
-# Recycle the toolkit instance
-del toolkit
-
-
-# %%
+#
 # Creating Custom Tools
 # --------------------------
 # A custom tool function must follow these rules:
@@ -121,6 +255,9 @@ del toolkit
 # - Typing for arguments
 # - Well-written docstring in Google style
 # - The return of the function must be wrapped by `ServiceResponse`
+#
+# After calling the `toolkit.add` function, the tool function will be parsed
+# automatically and registered to the `ServiceToolkit`.
 
 
 def new_function(arg1: str, arg2: int) -> ServiceResponse:
