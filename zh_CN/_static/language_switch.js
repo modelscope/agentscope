8d10d2e65b09ec7e669a5fa62a6e06768ebcfb63
DavdGao <DavdGao@users.noreply.github.com>
2025-05-11 09:01:37 +0000

    deploy: 324a56264edf74ab3c16ad2644cee386a2739259

---
+++ zh_CN/_static/language_switch.js
@@ -0,0 +1,9 @@
+// _static/language_switch.js
+
+function toChinese() {
+    window.location.href = "https://doc.agentscope.io/zh_CN";
+}
+
+function toEnglish() {
+    window.location.href = "https://doc.agentscope.io";
+}
