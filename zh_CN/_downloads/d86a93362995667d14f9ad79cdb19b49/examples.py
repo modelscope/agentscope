fdd520ed2c72a4ceea510e8203342747f800c186
DavdGao <DavdGao@users.noreply.github.com>
2025-08-13 14:25:40 +0000

    deploy: 33d33edc4c565563d89ee632fe4aeea6f70a0e78

---
+++ zh_CN/_downloads/d86a93362995667d14f9ad79cdb19b49/examples.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+"""
+样例
+========================
+
+配置类
+------------------------
+
+.. raw:: html
+
+    <a
+        class="gallery-item"
+        href="https://github.com/modelscope/agentscope/tree/main/examples/model_configs_template"
+    >
+        <div class="gallery-item-card">
+            <img
+                class="gallery-item-card-img"
+                src="https://img.alicdn.com/imgextra/i2/O1CN01M4Xm6S1PypUvcjzq5_!!6000000001910-0-tps-1598-1596.jpg"
+                alt="LLM 配置模版"
+            >
+            <div class="gallery-item-card-title"> LLM 配置模版 </div>
+        </div>
+        <div class="gallery-item-description">
+            在 AgentScope 中配置不同的LLM API。
+        </div>
+    </a>
+    <a
+        class="gallery-item"
+        href="https://github.com/modelscope/agentscope/tree/main/examples/model_llama3"
+    >
+        <div class="gallery-item-card">
+            <img
+                class="gallery-item-card-img"
+                src="https://img.alicdn.com/imgextra/i2/O1CN01M4Xm6S1PypUvcjzq5_!!6000000001910-0-tps-1598-1596.jpg"
+                alt="本地部署 LLaMA3"
+            >
+            <div class="gallery-item-card-title">
+                本地部署 LLaMA3
+            </div>
+        </div>
+        <div class="gallery-item-description">
+            使用 AgentScope 在本地设置 LLaMA3 模型服务。支持 CPU 和 GPU。
+        </div>
+    </a>
+
+------------------------
+
+智能体
+------------------------
+
+.. raw:: html
+
+    <a class="gallery-item">
+        <div class="gallery-item-card" href="https://github.com/modelscope/agentscope/tree/main/examples/conversation_with_web_browser_agent">
+            <img
+                class="gallery-item-card-img"
+                src="https://img.alicdn.com/imgextra/i2/O1CN01M4Xm6S1PypUvcjzq5_!!6000000001910-0-tps-1598-1596.jpg"
+                alt="Web 浏览器智能体"
+            >
+            <div class="gallery-item-card-title">
+                Web 浏览器智能体
+            </div>
+        </div>
+        <div class="gallery-item-description">
+            在 AgentScope 中构建一个 Web 浏览器控制智能体。
+        </div>
+    </a>
+
+    <a class="gallery-item" href="https://github.com/modelscope/agentscope/tree/main/examples/conversation_with_RAG_agents">
+        <div class="gallery-item-card">
+            <img
+                class="gallery-item-card-img"
+                src="https://img.alicdn.com/imgextra/i2/O1CN01M4Xm6S1PypUvcjzq5_!!6000000001910-0-tps-1598-1596.jpg"
+                alt="RAG 智能体"
+            >
+            <div class="gallery-item-card-title">RAG智能体</div>
+        </div>
+        <div class="gallery-item-description">
+            如何在 AgentScope 中使用 RAG。
+        </div>
+    </a>
+
+    <a class="gallery-item">
+        <div class="gallery-item-card" href="https://github.com/modelscope/agentscope/tree/main/examples/distributed_conversation">
+            <img
+                class="gallery-item-card-img"
+                src="https://img.alicdn.com/imgextra/i2/O1CN01M4Xm6S1PypUvcjzq5_!!6000000001910-0-tps-1598-1596.jpg"
+                alt="分布式智能体"
+            >
+            <div class="gallery-item-card-title">
+                分布式智能体
+            </div>
+        </div>
+        <div class="gallery-item-description">
+            如何在 AgentScope 中设置分布式智能体并与之交谈。
+        </div>
+    </a>
+
+------------------------
+
+游戏
+------------------------
+
+.. raw:: html
+
+    <a
+        class="gallery-item"
+        href="https://github.com/modelscope/agentscope/tree/main/examples/game_werewolf"
+    >
+        <div class="gallery-item-card">
+            <img
+                class="gallery-item-card-img"
+                src="https://img.alicdn.com/imgextra/i3/O1CN01dFpOh82643mygUh2Z_!!6000000007607-2-tps-1024-1024.png"
+                alt="狼人杀游戏"
+            >
+            <div class="gallery-item-card-title">
+                狼人杀游戏
+            </div>
+        </div>
+        <div class="gallery-item-description">
+            六个由 LLM 智能体进行狼人杀游戏。
+        </div>
+    </a>
+    <a class="gallery-item" href="https://github.com/modelscope/agentscope/tree/main/examples/game_gomoku">
+        <div class="gallery-item-card">
+            <img
+                class="gallery-item-card-img"
+                src="https://img.alicdn.com/imgextra/i1/O1CN01jKJ8rT28t1BKNy2Rz_!!6000000007989-2-tps-238-231.png"
+                alt="五子棋游戏"
+            >
+            <div class="gallery-item-card-title">五子棋游戏</div>
+        </div>
+        <div class="gallery-item-description">
+            智能体互相对弈五子棋游戏。
+        </div>
+    </a>
+
+
+------------------------
+
+对话
+------------------------
+
+.. raw:: html
+
+    <a class="gallery-item">
+        <div class="gallery-item-card" href="https://github.com/modelscope/agentscope/tree/main/examples/conversation_basic">
+            <img
+                class="gallery-item-card-img"
+                src="https://img.alicdn.com/imgextra/i2/O1CN01M4Xm6S1PypUvcjzq5_!!6000000001910-0-tps-1598-1596.jpg"
+                alt="基本对话"
+            >
+            <div class="gallery-item-card-title">
+                基本对话
+            </div>
+        </div>
+        <div class="gallery-item-description">
+            用户与智能体之间的基本对话。
+        </div>
+    </a>
+
+    <a class="gallery-item" href="https://github.com/modelscope/agentscope/tree/main/examples/conversation_with_mentions">
+        <div class="gallery-item-card">
+            <img
+                class="gallery-item-card-img"
+                src="https://img.alicdn.com/imgextra/i2/O1CN01M4Xm6S1PypUvcjzq5_!!6000000001910-0-tps-1598-1596.jpg"
+                alt="带@提及的对话"
+            >
+            <div class="gallery-item-card-title">
+                带@提及的对话
+            </div>
+        </div>
+        <div class="gallery-item-description">
+            在对话过程中，使用@来指定下一个发言者。
+        </div>
+    </a>
+
+"""
