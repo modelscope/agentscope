266717c54a5039773b628a757e638aded1426e45
DavdGao <gaodawei.gdw@alibaba-inc.com>
2025-08-19 13:41:37 +0800

    [WIP]

--- docs/tutorial/_static/language_switch.js
+++ docs/tutorial/_static/language_switch.js
@@ -5,3 +5,9 @@ function switchLanguage() {
         window.location.href = "https://doc.agentscope.io/zh_CN";
     }
 }
+
+
+function switchVersion() {
+    const suffix = window.location.href.includes("zh_CN") ? "/zh_CN" : "";
+    window.location.href = "https://doc.agentscope.io/v0" + suffix;
+}
