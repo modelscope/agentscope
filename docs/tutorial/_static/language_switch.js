8952d38057b3778292174b004d760f20650f9029
DavdGao <gaodawei.gdw@alibaba-inc.com>
2025-08-19 15:28:19 +0800

    finish

--- docs/tutorial/_static/language_switch.js
+++ docs/tutorial/_static/language_switch.js
@@ -1,9 +1,15 @@
-// _static/language_switch.js
-
-function toChinese() {
-    window.location.href = "https://doc.agentscope.io/zh_CN";
+function switchV0Language() {
+    if (window.location.href.includes("zh_CN")) {
+        window.location.href = "https://doc.agentscope.io/v0/en";
+    } else {
+        window.location.href = "https://doc.agentscope.io/v0/zh_CN";
+    }
 }
 
-function toEnglish() {
-    window.location.href = "https://doc.agentscope.io";
+function navigateToV1(version) {
+    if (version === "v1") {
+        const suffix = window.location.href.includes("zh_CN") ? "/zh_CN" : "";
+        window.location.href = "https://doc.agentscope.io" + suffix;
+    }
 }
+
